ecef790a40983579f72106a570b73d0c082c9f37
Iker Canut <ikermartincanut@gmail.com>
2021-12-03 13:01:33 -0300

    Parte 1

--- practica6_esqueleto.py
+++ practica6_esqueleto.py
@@ -1,6 +1,6 @@
 #! /usr/bin/python
 
-# 6ta Practica Laboratorio 
+# 6ta Practica Laboratorio
 # Complementos Matematicos I
 # Ejemplo parseo argumentos
 
@@ -43,7 +43,48 @@ class LayoutGraph:
         Aplica el algoritmo de Fruchtermann-Reingold para obtener (y mostrar)
         un layout
         """
-        pass
+        V, E = self.grafo
+        posiciones = {}
+
+        for v in V:
+            posiciones[v] = (np.random.randn(), np.random.randn())
+
+        for (a, b) in E:
+            x = [posiciones[a][0], posiciones[b][0]]
+            y = [posiciones[a][1], posiciones[b][1]]
+            plt.plot(x, y, marker='.', markersize=10)
+
+        plt.show()
+
+        accum = {}
+        for v in V:
+            accum[v] = [0, 0]
+
+        # Atraccion
+        for i in range(self.iters):
+            for (a, b) in E:
+                fx = posiciones[b][0] - posiciones[a][0]
+                fy = posiciones[b][1] - posiciones[a][1]
+                accum[a][0] += fx
+                accum[a][1] += fy
+                accum[b][0] -= fx
+                accum[b][1] -= fy
+
+        # Repulsion
+        for a in V:
+            for b in V:
+                if (a != b):
+                    fx = posiciones[a][0] - posiciones[b][0]
+                    fy = posiciones[a][1] - posiciones[b][1]
+                    accum[a][0] += fx
+                    accum[a][1] += fy
+                    accum[b][0] -= fx
+                    accum[b][1] -= fy
+
+        # TODO: VER BIEN LOS SIGNOS!!
+        # for v in V:
+        #    print(v + " " + str(posiciones[v][0]) + " " + str(posiciones[v][1]))
+        # print(accum)
 
 
 def main():
@@ -83,25 +124,50 @@ def main():
     print(args.iters)
     print(args.file_name)
     print(args.temp)
+    # return
+
+    grafo = lee_grafo_archivo(args.file_name)
+
+    # # Creamos nuestro objeto LayoutGraph
+    layout_gr = LayoutGraph(
+        grafo,
+        iters=args.iters,
+        refresh=1,
+        c1=0.1,
+        c2=5.0,
+        verbose=args.verbose
+    )
+
+    # # Ejecutamos el layout
+    layout_gr.layout()
     return
 
-    # # TODO: Borrar antes de la entrega
-    # grafo1 = ([1, 2, 3, 4, 5, 6, 7],
-    #           [(1, 2), (2, 3), (3, 4), (4, 5), (5, 6), (6, 7), (7, 1)])
-    #
-    # # Creamos nuestro objeto LayoutGraph
-    # layout_gr = LayoutGraph(
-    #     grafo1,  # TODO: Cambiar para usar grafo leido de archivo
-    #     iters=args.iters,
-    #     refresh=1,
-    #     c1=0.1,
-    #     c2=5.0,
-    #     verbose=args.verbose
-    # )
-    #
-    # # Ejecutamos el layout
-    # layout_gr.layout()
-    # return
+
+def lee_grafo_archivo(file_path):
+    '''
+    Lee un grafo desde un archivo y devuelve su representacion como lista.
+    Ejemplo Entrada: 
+        3
+        A
+        B
+        C
+        A B
+        B C
+        C B
+    Ejemplo retorno: 
+        (['A','B','C'],[('A','B'),('B','C'),('C','B')])
+    '''
+    with open(file_path, 'r') as f:
+        lines = f.readlines()
+        v = int(lines[0].strip())
+        V = []
+        for i in range(1, v+1):
+            V.append(lines[i].strip())
+        E = []
+        for i in range(v+1, len(lines)):
+            e = lines[i].strip().split(' ')
+            E.append((e[0], e[1]))
+        return (V, E)
 
 
 if __name__ == '__main__':
